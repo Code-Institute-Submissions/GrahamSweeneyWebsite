43aaa3b67e40ca452f48d234594217fa1cf3088a
Dave O'Dea <hi@daveodea.com>
2019-02-13 13:11:29 +0000

    Move JS scripts to own file

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+/* curator-feed-instagram */
+(function () {
+    var i, e, d = document,
+        s = "script";
+    i = d.createElement("script");
+    i.async = 1;
+    i.src = "https://cdn.curator.io/published/35a6722f-c836-49f2-8432-ce3945c63e26.js";
+    e = d.getElementsByTagName(s)[0];
+    e.parentNode.insertBefore(i, e);
+})();
